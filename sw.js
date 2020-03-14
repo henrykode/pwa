c73b562bb48b309b3927df76d28d89c353056c9f
Henry <kodenry@gmail.com>
2020-03-14 14:07:19 +0100

    delete comments

--- sw.js
+++ sw.js
@@ -12,17 +12,13 @@ self.addEventListener("install", function(event) {
   );
 });
 
-// If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener("fetch", function(event) {
   if (event.request.method !== "GET") return;
   event.respondWith(
     fetch(event.request)
       .then(function(response) {
         console.log("add page to offline cache: " + response.url);
-
-        // If request was success, add or update it in the cache
         event.waitUntil(updateCache(event.request, response.clone()));
-
         return response;
       })
       .catch(function(error) {
